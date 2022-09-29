769a2709db820d3bf32be542867b2291b020226a
nancycrojas <nanclarisaok@gmail.com>
2022-09-29 16:02:37 -0300

    Cambiando nombres de variables

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 //Crear un programa que pida al usuario ingresar, por separado, el nombre de una playlist y el título de tres canciones, y al finalizar se muestre un mensaje que diga, p. ej.: "Se ha creado la playlist 'Hits de los 80s' con las canciones 'Africa', 'Maniac', 'Final Countdown'".
 
-const playlist = prompt ("Por favor ingresa el nombre de tu playlist");
-const cancion1 = prompt ("Por favor ingresa la primer canción");
-const cancion2  = prompt ("Por favor ingresa la segunda canción");
-const cancion3  = prompt ("Por favor ingresa la tercera canción");
-alert(`Se ha creado la playlist "${playlist}" con las canciones "${cancion1}", "${cancion2}", "${cancion3}".`);
+let playlist = prompt("Por favor ingresa el nombre de tu playlist");
+let cancionUno = prompt("Por favor ingresa la primer canción");
+let cancionDos  = prompt("Por favor ingresa la segunda canción");
+let cancionTres  = prompt("Por favor ingresa la tercera canción");
+alert(`Se ha creado la playlist '${playlist}' con las canciones '${cancionUno}', '${cancionDos}', '${cancionTres}'.`);
 
-//alert("Se ha creado la playlist " +  playlist + " con las canciones " + cancion1 + ", " + cancion2 + ", " + cancion3);
+//alert("Se ha creado la playlist '" + playlist + "' con las canciones '" + cancionUno + "', '" + cancionDos + "', '" + cancionTres + "'.");
 
 
